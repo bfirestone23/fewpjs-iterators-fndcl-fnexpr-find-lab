e2c94694f0218ec38a8ee31215251a252f9ad196
Brian Firestone <bfirestone2339@gmail.com>
2021-03-24 16:47:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+]
 
+const superbowlWin = (array) => {
+    let result = array.find(i => i.result === 'W')
+    return !!result ? result.year : undefined
+};
